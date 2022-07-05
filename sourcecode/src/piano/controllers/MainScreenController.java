afb453831bff7bf1e2b348bc28b2272edae91ba6
dat <dat27072002@gmail.com>
2022-07-05 13:59:26 +0700

    update scene changing

--- sourcecode/src/piano/controllers/MainScreenController.java
+++ sourcecode/src/piano/controllers/MainScreenController.java
@@ -1,5 +1,61 @@
 package piano.controllers;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import piano.test.HelpScreen;
+import piano.test.PianoScreen;
+
 public class MainScreenController {
+	
+	private HelpScreen helpScreen;
+	private PianoScreen pianoScreen;
+	
+    @FXML
+    private Button helpButton;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    void helpButtonPressed(ActionEvent event) throws IOException {
+		final String HELP_FXML_FILE_PATH = "/piano/screen/fxmlSupporter/Help.fxml";
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(HELP_FXML_FILE_PATH));
+		HelpTextController helpText = new HelpTextController();
+		fxmlLoader.setController(helpText);
+		Parent root = fxmlLoader.load();	
+		
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("../screen/cssSupporter/HelpSupporter.css").toExternalForm());
+		
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Help");
+		primaryStage.getIcons().add(new Image("file:src/piano/picture/icons8-question-mark-30.png"));
+		primaryStage.centerOnScreen();
+		
+		primaryStage.initModality(Modality.APPLICATION_MODAL);
+		primaryStage.show();
+    }
+
+    @FXML
+    void playButtonPresed(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("/piano/screen/fxmlSupporter/Pianov2.fxml"));
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	Scene scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.setResizable(true);
+    	stage.centerOnScreen();
+    	stage.show();
+    }
 
 }
--- sourcecode/src/piano/view/MainScreen.java
+++ sourcecode/src/piano/view/MainScreen.java
@@ -2,28 +2,29 @@ package piano.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import piano.controllers.MainScreenController;
 
 public class MainScreen extends Application {
+	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../screen/fxmlSupporter/Main.fxml"));
+	public void start(Stage mainStage) throws Exception {
+		final String MAIN_FXML_FILE_PATH = "/piano/screen/fxmlSupporter/Main.fxml";
+		Parent root = FXMLLoader.load(getClass().getResource(MAIN_FXML_FILE_PATH));
+		
+		
+		Scene scene = new Scene(root);
 
-			Scene scene = new Scene(root);
-
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Piano");
-			primaryStage.getIcons().add(new Image("file:src/piano/picture/icon.png"));
-
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		mainStage.setScene(scene);
+		mainStage.setTitle("Piano");
+		mainStage.getIcons().add(new Image("file:src/piano/picture/icon.png"));
+		mainStage.setResizable(false);
+		mainStage.centerOnScreen();
+		mainStage.show();
 	}
 	
 	public static void main(String[] args) {
